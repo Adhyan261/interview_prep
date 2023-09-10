a6cd458255e70af4d80247050850003a60fc5d3c
Adhyan261 <sgupta.sg224@gmail.com>
2023-09-10 22:31:08 +0530

    new commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=xq_hv)qxc_sqvwr56rn-itx^d=+bb-l@8%&l=hbl6=htd!=#h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
